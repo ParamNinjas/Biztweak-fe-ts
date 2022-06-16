db9e1a47c3e6c556b12cec00663c77e63c574c59
KENNY_NGOIE <mukonkolekngoie@gmail.com>
2022-06-16 08:21:52 +0200

    Admid styling

--- src/Admin/AdminDash.tsx
+++ src/Admin/AdminDash.tsx
@@ -8,7 +8,8 @@ import SearchBar from "material-ui-search-bar";
 import Side from "./sideNav";
 import UserList from "./List";
 import { Api } from '../services/endpoints';
-import './AdminDash.css'    
+import './AdminDash.css'
+import { Bar } from "react-chartjs-2";
 
 
 
@@ -52,6 +53,52 @@ const Admin=() => {
                             <Typography variant='h3'>Overview</Typography>
                             <div className="bars">
                             <div className="bar1">
+                            <div className="App">
+      <h1>GEEKSFORGEEKS BAR CHART REACTJS</h1>
+      <div style={{ maxWidth: "650px" }}>
+        <Bar
+          data={{
+            // Name of the variables on x-axies for each bar
+            labels: ["1st bar", "2nd bar", "3rd bar", "4th bar"],
+            datasets: [
+              {
+                // Label for bars
+                label: "total count/value",
+                // Data or value of your each variable
+                data: [1552, 1319, 613, 1400],
+                // Color of each bar
+                backgroundColor: ["aqua", "green", "red", "yellow"],
+                // Border color of each bar
+                borderColor: ["aqua", "green", "red", "yellow"],
+                borderWidth: 0.5,
+              },
+            ],
+          }}
+          // Height of graph
+          height={400}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    // The y-axis value will start from zero
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+            legend: {
+              labels: {
+                fontSize: 15,
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+}
 
                             </div>
                             <div className="bar2">
