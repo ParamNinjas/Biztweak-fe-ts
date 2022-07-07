b2ee1c00e67ab55d150b7a6935006cbbb845354e
kgauzampe <kgauza.mpe@gmail.com>
2022-07-07 13:44:02 +0200

    testing

--- src/components/subscribe/Subscribe.tsx
+++ src/components/subscribe/Subscribe.tsx
@@ -4,25 +4,26 @@ import AOS from 'aos';
 import "aos/dist/aos.css";
 
 const Subscribe = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-    });
-  }, []);
+//   useEffect(() => {
+//     AOS.init({
+//       duration: 1000,
+//     });
+//   }, []);
   return (
-    <section id="subscribe">
-      <div className="container subscribe" data-aos="fade-up">
-      <h6>Newsletter</h6>
-        <h2>Let's Get in touch!</h2>
-        <p>Enter your email and become part of our entrepreneurial community. We want to engage with you about your business and opportunities.</p>
-        <form>
-          <div className="form-control">
-            <input type="text" placeholder="Enter Your Email..." />
-            <button>Subscribe</button>
-          </div>
-        </form>
-      </div>
-    </section>
+    <></>
+    // <section id="subscribe">
+    //   <div className="container subscribe" data-aos="fade-up">
+    //   <h6>Newsletter</h6>
+    //     <h2>Let's Get in touch!</h2>
+    //     <p>Enter your email and become part of our entrepreneurial community. We want to engage with you about your business and opportunities.</p>
+    //     <form>
+    //       <div className="form-control">
+    //         <input type="text" placeholder="Enter Your Email..." />
+    //         <button>Subscribe</button>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </section>
   );
 };
 
