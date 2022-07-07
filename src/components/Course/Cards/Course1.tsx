648101af82e0d9ed63814cf4bbb38b5dfe9e7cfd
kgauzampe <kgauza.mpe@gmail.com>
2022-07-07 14:44:08 +0200

    Build errors sorted

--- src/components/Course/Cards/Course1.tsx
+++ src/components/Course/Cards/Course1.tsx
@@ -5,7 +5,8 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import img from '../PDFS/course1.jpg';
-import pdf1 from '../../Course/PDFS/module1.pdf';
+// import pdf1 from '../../Course/PDFS/module1.pdf';
+import * as pdf1 from '../../Course/PDFS/module1.pdf';
 
 
 const btnClick = () => {
