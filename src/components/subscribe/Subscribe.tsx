8c1a785126a11cd9b26fd5470fb1827063af7d29
Kenny M. Ngoie <mukonkolekngoie@yahoo.com>
2022-07-07 08:12:11 +0200

    more stashing

--- src/components/subscribe/Subscribe.tsx
+++ src/components/subscribe/Subscribe.tsx
@@ -1,14 +1,46 @@
-import React, { useEffect } from "react";
+// 
+import { Component , useEffect} from "react";
 import "./Subscribe.css";
 import AOS from 'aos';
 import "aos/dist/aos.css";
 
-const Subscribe = () => {
+
+export default class form extends Component {
   useEffect(() => {
     AOS.init({
       duration: 1000,
     });
   }, []);
+
+const state={
+email: '',
+sent:false
+}
+
+//handler imput
+const handleEmail=(e)=>{
+this.setState({
+  email:e.target.email
+})
+}
+
+formSubmit=(e)=>{
+e.preventDefault();
+
+let data = {
+email:this.state.email
+}
+
+axios.post('/api/forms', data)
+.then(res=>){
+this.setState({
+  sent:true,
+    },this.resetForm())
+}).catch(()=>{
+console.log('message not sent');
+})
+}
+
   return (
     <section id="subscribe">
       <div className="container subscribe" data-aos="fade-up">
@@ -17,7 +49,14 @@ const Subscribe = () => {
         <p>Enter your email and become part of our entrepreneurial community. We want to engage with you about your business and opportunities.</p>
         <form>
           <div className="form-control">
-            <input type="text" placeholder="Enter Your Email..." />
+            <input 
+            type="text"
+            name="email"
+            className="email"
+            placeholder="Enter Your Email..."
+            value={this.state.email}
+            onChange={this.handleEmail}
+            />
             <button>Subscribe</button>
           </div>
         </form>
@@ -26,4 +65,3 @@ const Subscribe = () => {
   );
 };
 
-export default Subscribe;
