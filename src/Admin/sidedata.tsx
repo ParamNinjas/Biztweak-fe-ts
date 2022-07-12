a931aeb125051885385b8f9ae8837f6be979cc5f
Kenny M. Ngoie <mukonkolekngoie@yahoo.com>
2022-07-12 10:54:36 +0200

    3rd push

--- src/Admin/sidedata.tsx
+++ src/Admin/sidedata.tsx
@@ -7,7 +7,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import SubjectOutlined from ''
+// import  from ''
 
 
 // const barWidth = 240
